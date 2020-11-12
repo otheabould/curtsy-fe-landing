bae4fe428a47e165fde5f1333abf164065eab4b8
Oliver <oli@silverchip.com>
2020-11-12 09:43:38 +0000

    add input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState } from 'react';
+import './app.scss';
+
 const { default: Header } = require('./Header');
+const { default: TextInput } = require('./TextInput');
 
 function App() {
+    const [email, setEmail] = useState();
     return (
         <div className="App">
             <Header />
+            <div className="content">
+                <h1>All-in-one booking system</h1>
+                <h2>One tool for your whole team. Book, plan, and get organized.</h2>
+                <form>
+                    <TextInput
+                        placeholder="Enter your email..."
+                        type="email"
+                        value={email}
+                        onChange={(_, val) => {
+                            setEmail(val);
+                        }}
+                    />
+                </form>
+            </div>
         </div>
     );
 }
---
+++ src/TextInput.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+import './input.scss';
+
+function TextInput({ name, value, onChange, placeholder = '', type = 'text', required }) {
+    const [err, setError] = useState(validate(value));
+    const [showErr, setShowError] = useState(false);
+
+    return (
+        <div class="input">
+            <div class="input-container">
+                <input
+                    type="email"
+                    class="input-field"
+                    placeholder="Enter your email address"
+                    onChange={handleChange}
+                />
+                <div class="input-field-shadow"></div>
+            </div>
+            <div class="submit-container" style={{ opacity: value ? 1 : 0 }}>
+                <input type="submit" class="submit-btn" />
+                <div class="submit-btn-shadow"></div>
+            </div>
+        </div>
+    );
+
+    function handleBlur() {
+        setShowError(true);
+    }
+
+    function handleChange(e) {
+        e.preventDefault();
+
+        setError(validate(e.target.value));
+        onChange(name, e.target.value);
+    }
+
+    function validate(val) {
+        let error = '';
+
+        if (required && !val) {
+            error = 'This is a required field.';
+        } else if (type === 'email' && !validateEmail(val)) {
+            error = 'Please provide a valid email.';
+        }
+
+        return error;
+    }
+}
+
+function validateEmail(val) {
+    // eslint-disable-next-line
+    const reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return reg.test(val);
+}
+
+export default TextInput;
